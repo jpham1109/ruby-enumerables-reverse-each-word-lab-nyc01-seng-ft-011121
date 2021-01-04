2c66c59d3dbb9f0fa9a2a42f40689a7e766ec785
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-04 02:23:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,6 @@
 require 'pry'
 
 def reverse_each_word(string)
-  string = string.split(" ")
-  string.each do |element|
-    binding.pry
-    element.reverse 
-  end
-  string = string.join(" ")
-  string
+  string.split.each {|element| element.reverse
 end
 
